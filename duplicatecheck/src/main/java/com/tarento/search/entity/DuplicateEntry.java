add0a4be6a253cf46d5a4e751fce3f6c70e547ba
chethangowda <chethan007@gmail.com>
2019-09-17 20:39:15 +0530

    Removed unwated field from DuplicateEntry & removed unwanted files

--- duplicatecheck/src/main/java/com/tarento/search/entity/DuplicateEntry.java
+++ duplicatecheck/src/main/java/com/tarento/search/entity/DuplicateEntry.java
@@ -3,8 +3,6 @@
  */
 package com.tarento.search.entity;
 
-import java.util.UUID;
-
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Transient;
@@ -19,10 +17,8 @@ import javax.validation.constraints.Pattern;
 public class DuplicateEntry {
 	
 	@Id
-	private String id;
-
 	@NotNull
-	@Pattern(regexp = "^[A-Za-z0-9]{20}$")
+	@Pattern(regexp = "^[A-Za-z0-9]{20}$", message = "Input should be alphanumeric and length must be 20 chars")
 	private String alphanumericKey;
 	@Transient
 	private boolean state;
@@ -32,22 +28,13 @@ public class DuplicateEntry {
 	 */
 	public DuplicateEntry() {
 		super();		
-	}
-	
-	public String getId() {
-		return id;
-	}
-	
-	public void setId(String id) {
-		this.id = id;
-	}
+	}	
 		
 	/**
 	 * 
 	 */
 	public DuplicateEntry(String alphanumericKey) {
 		super();
-		this.id = UUID.randomUUID().toString();
 		this.alphanumericKey = alphanumericKey;
 	}
 	
@@ -70,12 +57,12 @@ public class DuplicateEntry {
 
 	@Override
 	public String toString() {
-		return "DuplicateEntry [id=" + getId() + ", duplicateKey=" + alphanumericKey + ", state="+getState()+"]";
+		return "DuplicateEntry [alphanumericKey=" + getAlphanumericEntry() + ", state="+getState()+"]";
 	}
     
 	@Override
 	public int hashCode() {
-		return id.hashCode();
+		return alphanumericKey.hashCode();
 	}
  
 	@Override
@@ -84,11 +71,11 @@ public class DuplicateEntry {
 			return true;
 		if (obj == null)
 			return false;
-		if (!(obj instanceof BaseEntity)) {
+		if (!(obj instanceof DuplicateEntry)) {
 			return false;
 		}
-		BaseEntity other = (BaseEntity) obj;
-		return getId().equals(other.getId());
+		DuplicateEntry other = (DuplicateEntry) obj;
+		return getAlphanumericEntry().equals(other.getAlphanumericEntry());
 	}
 
 
